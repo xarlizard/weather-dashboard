acbac02d6db0961f2e93169bd3dd2b8761cc490c
xarlizard <charkrios@gmail.com>
2024-11-12 19:56:30 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
-import axios from 'axios'
+import axios from "axios"
 
 const API_Key = "025eb857d7905a8da51fc4e27af4e6a9" //Free public api key, could be revoked
+//const time = Date.now();
 
+//const baseUrl = "https://api.openweathermap.org"
 const client = axios.create({
-  baseUrl: "https://api.openweathermap.org"
-})
+  baseURL: "https://api.openweathermap.org" 
+});
 
 function App() {
-  const [lat, setLat] = useState('0')
-  const [lon, setLon] = useState('0')
+  const [latlon, setLatlon] = useState([null, null]);
 
   function newCoords(position) {
     console.log(position.coords.latitude, position.coords.longitude)
-    setLat(position.coords.latitude)
-    setLon(position.coords.longitude)
+    setLatlon([position.coords.latitude, position.coords.longitude])
   }
   
-  function getLocation() {  
+  const getLocation = () => {  
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(newCoords);
     } else { 
@@ -26,20 +26,27 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    client
-      .get(`/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_Key}`)
-      .then((response)=>{
-        console.log(JSON.stringify(response.data))
-    })
-      .catch(()=>{
-        console.log("error");
+  const getWeather = () => {
+    if( latlon[0] != null && latlon[1] != null ) {
+      console.log("New location :", latlon);
+      client
+        .get(`/data/3.0/onecall?lat=${latlon[0]}&lon=${latlon[1]}&appid=${API_Key}`)
+        .then((response)=>{
+          console.log(response.data)
       })
-  }, [lat, lon]);
+        .catch(()=>{
+          console.log("error");
+      })
+    }
+    
+  }
   
   return (
     <>
       <button onClick={getLocation}>Get location</button>
+      <br />
+      <br />
+      <button onClick={getWeather}>Get weather</button>
     </>
   )
 }
