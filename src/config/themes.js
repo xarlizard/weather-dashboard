0e59538e03661c77a58ec6a2274bb15c1abce683
xarlizard <charkrios@gmail.com>
2025-05-19 22:53:53 +0200

    add theme config file

---
+++ src/config/themes.js
@@ -0,0 +1,12 @@
+// List of available themes for the app
+export const THEMES = [
+  { key: 'light', label: 'Light' },
+  { key: 'dark', label: 'Dark' },
+  { key: 'winter', label: 'Winter' },
+  { key: 'summer', label: 'Summer' },
+];
+
+export const BASIC_THEMES = [
+  { key: 'light', label: 'Light' },
+  { key: 'dark', label: 'Dark' },
+];
